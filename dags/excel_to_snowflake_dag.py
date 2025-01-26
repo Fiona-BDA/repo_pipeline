b72423ab63e5b4b553cb4d0b7c6d1754ed656cc2
Fiona-BDA <fionanijiang@gmail.com>
2025-01-26 09:54:14 -0500

    Fiona_2_sheets

--- dags/excel_to_snowflake_dag.py
+++ dags/excel_to_snowflake_dag.py
@@ -3,6 +3,9 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 from etl.excel_to_snowflake_etl import excel_to_snowflake_etl
 from etl.new_task import new_task_function
+# add the new function here 
+from etl.new_task_Fiona import new_task_function_Fiona
+
 from etl.ETL_S3_SNOWFLAKES import execute_snowflake_sql
 from etl.sf_to_postgresql import sf_to_postgresql
 from dotenv import load_dotenv
@@ -23,7 +26,12 @@ with DAG(
         task_id="new_task",
         python_callable=new_task_function
     )
+    SHAMPOO_task = PythonOperator(
+        task_id="SHAMPOO_task",
+        python_callable= new_task_function_Fiona
+    )
+
 
     # Set task dependencies
-    etl_task >> new_task
+    etl_task >> new_task >> SHAMPOO_task
 
