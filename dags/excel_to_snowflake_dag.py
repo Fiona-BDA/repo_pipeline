a76feaecd9570502d8edbf55b00da4524851132e
Fiona-BDA <fionanijiang@gmail.com>
2025-01-26 09:54:14 -0500

    v26.1

--- dags/excel_to_snowflake_dag.py
+++ dags/excel_to_snowflake_dag.py
@@ -23,7 +23,12 @@ with DAG(
         task_id="new_task",
         python_callable=new_task_function
     )
+    SHAMPOO_task = PythonOperator(
+        task_id="SHAMPOO_task",
+        python_callable= new_task_function_Fiona
+    )
+
 
     # Set task dependencies
-    etl_task >> new_task
+    etl_task >> new_task >> SHAMPOO_task
 
